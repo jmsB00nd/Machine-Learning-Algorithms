e5af637517ba6e1a36117ab4bf70cedb91507727
jmsB00nd <arabhamza4349@gmail.com>
2025-01-01 12:48:30 +0100

    added Layer class

---
+++ Neural Network/NN/Layer.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from Neuron import Neuron
+from Activation import Logistic
+
+
+def backpropagate_layer(neurons, delta_next, w_next, alpha=0.1):
+    w_pasts = []
+    deltas = []
+    
+    for i,neuron in enumerate(neurons):
+        delta, w_past = neuron.update(delta_next, w_next[i], alpha=alpha)
+        w_pasts.append(w_past)
+        deltas.append(delta)
+        
+    return np.array(deltas).T, np.array(w_pasts).T
+
+class Layer(object):
+    def __init__(self,size, in_size, activation=Logistic()):
+        self.neurons = [Neuron(in_size, activation) for i in range(size)]
+        
+    def forward(self, X):
+        activations = []
+        for neuron in self.neurons:
+            activations.append(neuron.activate(X))
+        return np.array(activations).T
+    
+    def backward(self, Delta_next, W_next, alpha=1.):
+        return backpropagate_layer(self.neurons, Delta_next, W_next, alpha=alpha)
+ 
--- Neural Network/NN/test.py
+++ Neural Network/NN/test.py
@@ -1,37 +1,29 @@
 import numpy as np
 from Activation import Logistic, ReLU
 from Neuron import Neuron
-
-# New neuron with two inputs
-n = Neuron(2)
-# ---------------------
-# We should not affect the weights directly
-# Here, it's just to have the same weights of the output neuron in the example seen in the lecture
-# We will reproduce the parameters of neuron 1 hidden layer 1 (layer 2)
-n.b = -0.3
-n.w = np.array([0.5, 0.2])
-# ---------------------
-
-# M X Lp (Here A1 = X; the input)
-A1 = np.array([[2., -1.], [3., 5.]])
-# M X Ln (Delta of the next layer)
-Delta3 = np.array([[ 0.14523862, -0.02613822], [ 0.1394202, -0.02531591]]).T
-W3_1 = np.array([0.3, -0.1])
+from Layer import Layer
 
 
-A2_1 = n.activate(A1)
-print("z2_1 = " + str(n.z))
-print("a2_1 = " + str(A2_1))
-# The partial derivative of logistic function does not need Z, so we pass 0 (to not calculte it)
-print("partial(a2_1) = " + str(n.activation.partial(0, A2_1)))
-print("past b = " + str(n.b))
+# layer 2 (2 neurons, 2 inputs)
+c2 = Layer(2, 2)
 
-Delta2, W2_past = n.update(Delta3, W3_1) 
+# We should not directly assign  weights 
+# Here, it is just to have the same weights of the output neuron of the lecture's example
+c2.neurons[0].b = -0.3
+c2.neurons[0].w = np.array([0.5, 0.2])
+c2.neurons[1].b = 0.5
+c2.neurons[1].w = np.array([0.3, 0.4])
 
-print("past w = " + str(W2_past))
-print("delta2 = " + str(Delta2))
-print("new b = " + str(n.b))
-print("new w = " + str(n.w))
+delta3 = np.array([[ 0.14523862, -0.02613822], [ 0.1394202, -0.02531591]]).T
+w3 = np.array([[0.3, -0.1],[0.5, -0.3]])
+
+a1 = np.array([[2., -1.], [3., 5.]])
+a2 = c2.forward(a1)
+print("Activations: " + str(a2))
+
+Deltas2, W_pasts2 = c2.backward(delta3, w3)
+
+print("Deltas: " + str(Deltas2))
 
 
 
